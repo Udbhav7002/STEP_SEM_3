64b2bcdc93d0c206584973558a7e1994d01ff41e
Udbhav <sanagariudbhav@gmail.com>
2026-09-12 16:28:16 +0530

    Week 2: Add assignment problems (Problem1-Problem5)

---
+++ Problem2_WordReversalEncoder.java
@@ -0,0 +1,29 @@
+public class Problem2_WordReversalEncoder {
+    public static String reverseEachWord(String sentence) {
+        if (sentence == null || sentence.isEmpty()) {
+            return "";
+        }
+
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            StringBuilder reversedWord = new StringBuilder();
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reversedWord.append(word.charAt(j));
+            }
+            result.append(reversedWord);
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        System.out.println(result.toString());
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        reverseEachWord("hello club");
+    }
+}
